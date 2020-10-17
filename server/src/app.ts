10be7bfac20b3a9ae461d58705cdae4560a89372
Sam <super8989@gmail.com>
2020-10-17 15:39:21 -0400

    Add client side

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,15 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import todoRoutes from './routes';
 
+
 const app: Express = express();
+const morgan = require('morgan')
 
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(todoRoutes);
+app.use(morgan('dev'))
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@merntodo.g5pzm.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
